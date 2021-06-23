1942ee900853bc80816a7516c1ec8d665c089fdc
glendo <gleneggers64@gmail.com>
2021-06-23 15:53:47 -0500

    add structure for footer element

---
+++ src/components/Footer/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import MediaLink from '../MediaLink';
+
+function Footer() {
+	const media = [
+		{
+			label: "github.com/glendonintendo",
+			url: "https://github.com/glendonintendo/",
+			icon: "fab fa-github"
+		},
+		{
+			label: "linkedin.com/in/gleneggers/",
+			url: "https://www.linkedin.com/in/gleneggers/",
+			icon: "fab fa-linkedin"
+		},
+		{
+			label: "instagram.com/gleneggers64",
+			url: "https://www.instagram.com/gleneggers64/",
+			icon: "fab fa-instagram"
+		},
+		{
+			label: "twitter.com/eggers_glen",
+			url: "https://twitter.com/eggers_glen",
+			icon: "fab fa-twitter"
+		}
+	];
+
+	return (
+		<>
+			<section className='connect'>
+				<h2>Want to Connect?</h2>
+				<ul>
+					{media.map(medium => <MediaLink mediaData={medium} />)}
+				</ul>
+			</section>
+			<footer>
+				<p>Created and developed with ❤️ by Glen Eggers</p>
+			</footer>
+		</>
+	)
+};
+
+export default Footer;
