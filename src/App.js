c449e9172f2b22d5ca833922012be403de37f0f8
glendo <gleneggers64@gmail.com>
2021-06-29 17:02:59 -0500

    add About Portfolio and Contact components to main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio'
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
       <Nav />
 	  <main>
-
+		<About />
+		<Portfolio />
+		<Contact />
 	  </main>
 	  <Footer />
     </div>
