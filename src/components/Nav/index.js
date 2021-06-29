448eb5e4cfcaf73fa782f47ed5df16bb1196aa98
glendo <gleneggers64@gmail.com>
2021-06-29 17:00:17 -0500

    add skeleton for Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Nav() {
+	return (
+		<header>
+			<h1>#glen</h1>
+			<nav>
+				<ul>
+					<li>.about</li>
+					<li>.projects</li>
+					<li>.contact</li>
+					<li>.resume</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Nav;
