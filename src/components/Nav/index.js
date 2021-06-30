56fbab0f00b8e1ebfbbee470354cec4e5e364c2b
glendo <gleneggers64@gmail.com>
2021-06-29 19:04:31 -0500

    add custom navigation active styles

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Nav() {
+	const checkActive = (match, location) => {
+		if (location.pathname !== '/portfolio' && location.pathname !== '/contact') {
+			return true;
+		} else {
+			return false;
+		}
+	};
+	
 	return (
 		<header>
 			<h1><Link to='/'>#glen</Link></h1>
 			<nav>
 				<ul>
-					<li><Link to='/about'>.about</Link></li>
-					<li><Link to='/portfolio'>.portfolio</Link></li>
-					<li><Link to='/contact'>.contact</Link></li>
+					<li><NavLink to='/about' activeClassName='nav-selected' isActive={checkActive}>.about</NavLink></li>
+					<li><NavLink to='/portfolio' activeClassName='nav-selected'>.portfolio</NavLink></li>
+					<li><NavLink to='/contact' activeClassName='nav-selected'>.contact</NavLink></li>
 					<li><a href={require('../../assets/web-dev-resume.pdf')?.default} target='_blank'>.resume</a></li>
 				</ul>
 			</nav>
