6fb898d0c6b784e63deebd7146179b0ec87d4e63
glendo <gleneggers64@gmail.com>
2021-06-29 20:03:04 -0500

    complete styles for About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,12 +8,12 @@ function About() {
 				src={require('../../assets/profile-pic.jpg')?.default}
 				alt='profile of Glen'	
 			/>
-			<div calss='about-text'>
+			<div className='about-text'>
 				<h2>Hello, I'm Glen</h2>
 				<p>
 					I am a full stack web developer currently based in Austin, TX.
 					<br /> <br />
-					I develop applications using HTML(5), CSS(3), Javascript, and JQuery. I also have experience managing databases with SQL and developing powerful single-use programs with Python.
+					I develop powerful, single-page, progressive web applications using the MongoDB, Express, React, Node (MERN) tech stack.
                 	<br /> <br />
                 	I want to work with you to create beautiful, responsive applications that both fit your organization's needs and exceed your expectations.
 				</p>
